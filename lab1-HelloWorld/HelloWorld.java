3ffd3dd883647962cae7d76cb093df4bd3e02798
xim217 <xim217@lehigh.edu>
2015-01-23 16:51:50 +0000

    updated HelloWorld

---
+++ lab1-HelloWorld/HelloWorld.java
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////////////
+//Xintong MIng
+//lab01
+//Hello World Java Program
+//
+// first compile the program
+//      javac HelloWorld.java
+// run the program
+//      java HelloWorld//
+
+// define a class
+public class HelloWorld{
+    
+//   add main method    
+    public static void main(String[] arge) {
+        
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("#                    #");
+        System.out.println("# Hello World 2014!! #");
+        System.out.println("#                    #");
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("test");
+    }
+    
+}
