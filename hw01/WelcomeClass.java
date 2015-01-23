cea8310c3aba32aecf0e6eaa20760aa58d282f23
xim217 <xim217@lehigh.edu>
2015-01-23 18:46:20 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////////////
+//Xintong MIng
+//hw01
+//Welcome Class Java Program
+//
+// first compile the program
+//      javac WelcomeClass.java
+// run the program
+//      java WelcomeClass.java//
+
+// define a class
+public class WelcomeClass{
+    
+//   add main method    
+    public static void main(String[] arge) {
+        
+        System.out.println("   -----------   ");
+        System.out.println("   | WELCOME |   ");
+        System.out.println("   -----------   ");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-E--J--K--0--0--0->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+    }
+    
+}
