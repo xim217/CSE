ee1b993e95a62592f3d5a299e47680d731705bee
xim217 <xim217@lehigh.edu>
2015-02-09 16:29:06 +0000

    updtaed Root

--- hw03/Root.java
+++ hw03/Root.java
@@ -22,6 +22,6 @@
    			     guess4 = (2*guess3*guess3*guess3+x)/(3*guess3*guess3);
    			     guess5 = (2*guess4*guess4*guess4+x)/(3*guess4*guess4);// improve the estimation for four times
    			     cube = guess5*guess5*guess5;
-   			     System.out.print(" Coub root is " +guess4+" and the value of this crude guess when cubed is "+cube+" ");
+   			     System.out.print(" Coub root is " +guess4+" and the value of this crude guess when cubed is " +guess4+"*"+guess4+"*"+guess4+"="+cube+" ");   
    			  }
  }
